31a6fba512d351d673b8a3111d3b40b37dea1f8a
Kyle Price <kyleprice@yahoo.com>
2020-05-16 17:19:53 -0400

    Initial code commit

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { startLogout } from '../actions/auth';
+import { connect } from 'react-redux';
+
+export const Header = ({ logout }) => (
+    <header className="header">
+        <div className="content-container">
+            <div className="header__content">
+                <Link className="header__title" to='/dashboard'>
+                    <h1>Boilerplate</h1>
+                </Link>
+                <button
+                    className="button button--link"
+                    onClick={logout}
+                >
+                    Logout
+                </button>
+            </div>
+        </div>
+    </header>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+    logout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
